2685e3feadf3b6637e44421859d8b9282c9d335a
yangxiaoxiao <yangxiaoxiao@iqianjin.com>
2019-02-21 17:08:24 +0800

    idea快捷键写出打印0-99的for循环

---
+++ src/main/java/ForTest.java
@@ -0,0 +1,10 @@
+//psvm  回车
+//100.fori 回车
+//i.sout 回车
+public class ForTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
